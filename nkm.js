3eeee2d258fe342d8fe9bbeeea818d62e0f0e3a0
Maksymilian Bolek <maksymilian.bolek@schibsted.pl>
2016-02-13 02:54:50 +0100

    Hello world

---
+++ nkm.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         nkm
+// @namespace    nkm
+// @version      0.1
+// @description  Archive your messages
+// @author       Maksymilian Bolek
+// @homepage     https://github.com/makbol/nkm
+// @supportURL   https://github.com/makbol/nkm/issues
+// @match        http://nk.pl/*
+// @grant        none
+// @run-at       document-idle
+// @require      https://cdn.jsdelivr.net/g/momentjs@2.11.2,jquery@2.2.0,lodash@4.3.0
+// ==/UserScript==
+
+// Hai!
